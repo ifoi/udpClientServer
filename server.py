51ff0e384095c05d681c3b08420b728a70ac98f8
Ifo Ikede <ifoitech@gmail.com>
2022-03-18 16:37:50 -0300

    add catch for ctrl+c

--- server.py
+++ server.py
@@ -25,23 +25,33 @@ def startServer():
     #  bind to all interfaces
     server_socket.bind((serverIP, localPort))
 
-    print("UDP server started   ")
+    print("  *** UDP server started ***  \n *********** \n use Ctrl-c to exit the program!! ")
 
-    # recieved data from client and send back reply
-    while True:
-        data, addr = server_socket.recvfrom(bufferSize)
-        print("recieved from client: " + str(data.decode("utf-8")))
-        clientInput = str(data.decode("utf-8"))
-        try:
-            int(clientInput)  # checks if input is a number
-            msg = str(int(clientInput) * 2)
-        except ValueError:
-            try:  # check if input is a float
-                float(clientInput)
-                msg = str(float(clientInput) * 2)
+    try:
+        # recieved data from client and send back reply
+        while True:
+            data, addr = server_socket.recvfrom(bufferSize)
+            print("recieved from client: " + str(data.decode("utf-8")))
+            clientInput = str(data.decode("utf-8"))
+            try:
+                int(clientInput)  # checks if input is a number
+                msg = str(int(clientInput) * 2)
             except ValueError:
-                msg = "Error: input must be a number"
-        server_socket.sendto(msg.encode("utf-8"), addr)
+                try:  # check if input is a float
+                    float(clientInput)
+                    msg = str(float(clientInput) * 2)
+                except ValueError:
+                    msg = "Error: input must be a number"
+            server_socket.sendto(msg.encode("utf-8"), addr)
+
+ # catches Ctr-c or keyboard interrrupt
+    except KeyboardInterrupt:
+        print()
+        response = input(
+            "Ctrl-c was pressed. Do you really want to exit? y/n ")
+        if response == "y":
+            server_socket.close()
+            exit(1)
 
 
 startServer()
